5d5e6586526672c9a9f9c77bb1c935378279441b
mustafamuhamedahmed <133133697+mustafamuhamedahmed@users.noreply.github.com>
2025-01-14 20:18:48 +0200

    Update App.js

--- App.js
+++ App.js
@@ -2,29 +2,37 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+
 import Home from "./pages/Home";
 import Shop from "./pages/Shop";
 import ProductDetailsPage from "./pages/ProductDetailsPage";
-import Profile from "./pages/Profile";
 import Cart from "./pages/Cart";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
-import Admin from "./pages/admin/Admin";
-import Dashboard from "./pages/admin/Dashboard";
-import Orders from "./pages/admin/Orders";
-import EditProfile from "./pages/EditProfile";
+import Admin from "./pages/Admin";
+import Dashboard from "./pages/Dashboard";
+import Orders from "./pages/Orders";
+import ViewDetails from "./pages/ViewDetails";
+
 import Payment from "./pages/Payment";
 import PostCoupons from "./pages/PostCoupons";
 import Checkout from "./pages/Checkout";
 import About from "./pages/About";
-import PrivateRoute from "./components/PrivateRoute"; // حماية المسارات الخاصة
-import { ToastContainer } from "react-toastify";
+import { ToastContainer } from 'react-toastify'; 
 
 import "./styles/global.css";
 import "./styles/variables.css";
 import "./App.css";
+import { formatPrice } from "./utils/formatPrice";
+import { SPORTS_CATEGORIES, TAX_RATE } from "./utils/constants";
 import { AuthProvider } from "./contexts/AuthContext";
 import { CartProvider } from "./contexts/CartContext";
+
+console.log(SPORTS_CATEGORIES);
+console.log(`Tax Rate: ${TAX_RATE * 100}%`);
+
+const price = 99.99;
+console.log(formatPrice(price, "USD"));
 
 const App = () => {
   return (
@@ -38,24 +46,23 @@ const App = () => {
                 <Route path="/" element={<Home />} />
                 <Route path="/shop" element={<Shop />} />
                 <Route path="/products/:productId" element={<ProductDetailsPage />} />
-                <Route path="/profile" element={<PrivateRoute><Profile /></PrivateRoute>} />
                 <Route path="/cart" element={<Cart />} />
                 <Route path="/login" element={<Login />} />
                 <Route path="/register" element={<Register />} />
-                <Route path="/admin" element={<PrivateRoute><Admin /></PrivateRoute>} />
-                <Route path="/dashboard" element={<PrivateRoute><Dashboard /></PrivateRoute>} />
-                <Route path="/orders" element={<PrivateRoute><Orders /></PrivateRoute>} />
-                <Route path="/edit-profile" element={<PrivateRoute><EditProfile /></PrivateRoute>} />
+                <Route path="/admin" element={<Admin />} /> 
+                <Route path="/dashboard" element={<Dashboard />} />
+                <Route path="/orders" element={<Orders />} />
+                <Route path="/view-details" element={<ViewDetails />} />
+
                 <Route path="/payment" element={<Payment />} />
-                <Route path="/post-coupons" element={<PrivateRoute><PostCoupons /></PrivateRoute>} />
+                <Route path="/post-coupons" element={<PostCoupons />} />
                 <Route path="/checkout" element={<Checkout />} />
-                <Route path="/about" element={<About />} />
-                <Route path="*" element={<NotFound />} />
+                <Route path="/about" element={<About />} /> 
               </Routes>
             </main>
             <Footer />
           </div>
-          <ToastContainer />
+          <ToastContainer /> 
         </CartProvider>
       </AuthProvider>
     </Router>
@@ -63,3 +70,4 @@ const App = () => {
 };
 
 export default App;
+
