0ad7d2938481452d886e16a1374009dbf2b01e99
mustafamuhamedahmed <133133697+mustafamuhamedahmed@users.noreply.github.com>
2025-01-15 18:46:31 +0200

    Update ProductCard.jsx

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -9,12 +9,12 @@ const ProductCard = ({ name, price, image, onAddToCart, onProductClick }) => {
         src={image}
         alt={name}
         className="product-card__image"
-        onClick={onProductClick} // إضافة حدث النقر على الصورة
-        style={{ cursor: "pointer" }} // تغيير مؤشر الفأرة لإظهار أن الصورة قابلة للنقر
+        onClick={onProductClick} 
+        style={{ cursor: "pointer" }} 
       />
       <div className="product-card__details">
         <h3 className="product-card__name" onClick={onProductClick} style={{ cursor: "pointer" }}>
-          {name} {/* يمكنك جعل الاسم أيضًا قابل للنقر */}
+          {name} 
         </h3>
         <p className="product-card__price">${price.toFixed(2)}</p>
         <Button onClick={onAddToCart} label="Add to Cart" />
